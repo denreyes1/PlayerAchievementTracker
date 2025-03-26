54be8ef74655fc308297af9c80d8af2a514c189a
Densho <denj.rey.c@gmail.com>
2025-03-26 01:42:55 -0400

    Changed user-authentication-service to use apollo sandbox

--- user-authentication-service/server.js
+++ user-authentication-service/server.js
@@ -1,90 +1,50 @@
-require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const connectDB = require('./db');
-const { signup, login } = require('./auth');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const typeDefs = require('./schemas/user.server.schema');
+const resolvers = require('./resolvers/user.server.resolvers');
 
-// Define GraphQL schema
-const typeDefs = gql`
-    type User {
-        id: ID!
-        username: String!
-        email: String!
-        role: String!
-        createdAt: String!
-    }
 
-    type AuthPayload {
-        token: String
-        user: User
-    }
+async function startServer() {
+  dotenv.config();
 
-    type Query {
-        me: User
-    }
-
-    type Mutation {
-        signup(username: String!, email: String!, password: String!): AuthPayload
-        login(email: String!, password: String!): AuthPayload
-        logout: Boolean
-    }
-`;
-
-// Define resolvers
-// In-memory token blacklist (for demonstration purposes)
-let tokenBlacklist = [];
-
-const resolvers = {
-  Mutation: {
-    signup: async (_, { username, email, password }) => {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = new User({ username, email, password: hashedPassword });
-      await user.save();
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-      return { token, user };
-    },
-    login: async (_, { email, password }) => {
-      const user = await User.findOne({ email });
-      if (!user) {
-        throw new Error('No user found with this email');
-      }
-      const valid = await bcrypt.compare(password, user.password);
-      if (!valid) {
-        throw new Error('Invalid password');
-      }
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-      return { token, user };
-    },
-    logout: (_, __, { token }) => {
-      // Add the token to the blacklist
-      tokenBlacklist.push(token);
-      return true;
-    },
-  },
-};
-
-// Middleware to check for blacklisted tokens
-const checkTokenBlacklist = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (token && tokenBlacklist.includes(token)) {
-    return res.status(401).send('Token is invalidated');
-  }
-  next();
-};
-
-// Start the server
-const startServer = async () => {
   const app = express();
-  app.use(checkTokenBlacklist); // Use the middleware to check for blacklisted tokens
-  const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ token: req.headers.authorization?.split(' ')[1] }) });
-  await server.start();
-  server.applyMiddleware({ app });
+  
+  // Middleware
+  app.use(cookieParser());
+  app.use(express.json());
+  // app.use(
+  //     cors({
+  //         origin: [process.env.CLIENT_URL, 'http://localhost:3000', 'http://localhost:5174'],
+  //         credentials: true,
+  //     })
+  // );
+  
+  // Connect to MongoDB
+  mongoose.connect('mongodb://0.0.0.0:27017/DenisjannReyes-lab-assignment3', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err));
+  // Create a new ApolloServer instance and pass the schema data
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({ req, res }),
+  });
 
-  await connectDB(); // Connect to MongoDB
+  await apolloServer.start();
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
-};
+  // Apply Apollo GraphQL middleware and specify the path to /graphql
+  apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+  const PORT = 4000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/graphql`);
+  });
+}
 
 startServer();
